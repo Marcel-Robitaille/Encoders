12a0a9ffb22b4df334c5c61c25ae306621a178e0
Marcel Robitaille <marcelrobitaille11@gmail.com>
2016-02-26 21:29:43 -0400

    Moved everything that didnt need to be exposed under private

--- Encoders.h
+++ Encoders.h
@@ -3,57 +3,9 @@ class Encoders{
 	int pinRA, pinRB, pinLA, pinLB;
 	int R_A_SIG=0, R_B_SIG=1, L_A_SIG=0, L_B_SIG=1;
 
-	static Encoders * instance;
-
-	static void R_A_RISE(){
-		if(Encoders::instance != NULL){
-			Encoders::instance->switchPressed(Encoders::instance->getPinRA(), 1);
-		}
-	}
-
-	static void R_A_FALL(){
-		if(Encoders::instance != NULL){
-			Encoders::instance->switchPressed(Encoders::instance->getPinRA(), 0);
-		}
-	}
-	
-	static void R_B_RISE(){
-		if(Encoders::instance != NULL){
-			Encoders::instance->switchPressed(Encoders::instance->getPinRB(), 1);
-		}
-	}
-
-	static void R_B_FALL(){
-		if(Encoders::instance != NULL){
-			Encoders::instance->switchPressed(Encoders::instance->getPinRB(), 0);
-		}
-	}
-
-	static void L_A_RISE(){
-		if(Encoders::instance != NULL){
-			Encoders::instance->switchPressed(Encoders::instance->getPinLA(), 1);
-		}
-	}
-
-	static void L_A_FALL(){
-		if(Encoders::instance != NULL){
-			Encoders::instance->switchPressed(Encoders::instance->getPinLA(), 0);
-		}
-	}
-	
-	static void L_B_RISE(){
-		if(Encoders::instance != NULL){
-			Encoders::instance->switchPressed(Encoders::instance->getPinLB(), 1);
-		}
-	}
-
-	static void L_B_FALL(){
-		if(Encoders::instance != NULL){
-			Encoders::instance->switchPressed(Encoders::instance->getPinLB(), 0);
-		}
-	}
-	
 	public:
+		static Encoders * instance;
+
 		void begin (const int _pinRA, const int _pinRB, const int _pinLA, const int _pinLB){
 			pinRA = _pinRA;
 			pinRB = _pinRB;
@@ -66,16 +18,67 @@ class Encoders{
 
 			attachInterrupt(pinLA, L_A_RISE, RISING);
 			attachInterrupt(pinLB, L_B_RISE, RISING);
+			
 			instance = this;
 		}
 
 		long getRightCount(){
 			return RightCount;
 		}
+
 		long getLeftCount(){
 			return LeftCount;
 		}
 
+	private:
+		static void R_A_RISE(){
+			if(Encoders::instance != NULL){
+				Encoders::instance->switchPressed(Encoders::instance->getPinRA(), 1);
+			}
+		}
+
+		static void R_A_FALL(){
+			if(Encoders::instance != NULL){
+				Encoders::instance->switchPressed(Encoders::instance->getPinRA(), 0);
+			}
+		}
+		
+		static void R_B_RISE(){
+			if(Encoders::instance != NULL){
+				Encoders::instance->switchPressed(Encoders::instance->getPinRB(), 1);
+			}
+		}
+
+		static void R_B_FALL(){
+			if(Encoders::instance != NULL){
+				Encoders::instance->switchPressed(Encoders::instance->getPinRB(), 0);
+			}
+		}
+
+		static void L_A_RISE(){
+			if(Encoders::instance != NULL){
+				Encoders::instance->switchPressed(Encoders::instance->getPinLA(), 1);
+			}
+		}
+
+		static void L_A_FALL(){
+			if(Encoders::instance != NULL){
+				Encoders::instance->switchPressed(Encoders::instance->getPinLA(), 0);
+			}
+		}
+		
+		static void L_B_RISE(){
+			if(Encoders::instance != NULL){
+				Encoders::instance->switchPressed(Encoders::instance->getPinLB(), 1);
+			}
+		}
+
+		static void L_B_FALL(){
+			if(Encoders::instance != NULL){
+				Encoders::instance->switchPressed(Encoders::instance->getPinLB(), 0);
+			}
+		}
+	
 		int getPinRA(){
 			return pinRA;
 		}
@@ -95,6 +98,7 @@ class Encoders{
 
 		void switchPressed(int pin, int rise){
 			detachInterrupt(pin);
+
 			if(pin == pinRA){
 				R_A_SIG = rise;
 				if(R_B_SIG == rise){
